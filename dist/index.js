52210af237bf50698d81063f67ba21f54f608ac0
onelson-main <onelson.main@gmail.com>
2022-06-18 00:54:25 +0100

    fixed export related crash_3

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,18 @@
-export { default as generateVerificationMail } from './mails/verifyMail';
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+exports.__esModule = true;
+exports.generateVerificationMail = void 0;
+var verifyMail_1 = require("./mails/verifyMail");
+__createBinding(exports, verifyMail_1, "default", "generateVerificationMail");
 // export default {generateVerificationMail}
 // console.log(generateVerificationMail())
